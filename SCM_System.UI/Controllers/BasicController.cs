51e998991b9fa410b52c6258bb0c716ebbf6b339
朋朋 王 <879173660@qq.com>
2019-12-19 18:08:28 +0800

    重新添加UI

---
+++ SCM_System.UI/Controllers/BasicController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM_System.UI.Controllers
+{
+    public class BasicController : Controller
+    {
+        // GET: basic
+        public ActionResult Customer()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerAdd()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult ProductAdd()
+        {
+            return View();
+        }
+
+        public ActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult StoreAdd()
+        {
+            return View();
+        }
+
+        public ActionResult storeEdit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SCM_System.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCM_System.UI.MvcApplication" Language="C#" %>
