584cbfbce7ed801c17be148825eee83b30e65adb
朋朋 王 <879173660@qq.com>
2019-12-19 14:44:24 +0800

    UI文档

---
+++ SCM_System.UI/Views/AssembleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM_System.UI.Views
+{
+    public class AssembleController : Controller
+    {
+        // GET: Assemble
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SCM_System.UI/Views/Pdskc/indexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM_System.UI.Views.Pdskc
+{
+    public class indexController : Controller
+    {
+        // GET: index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Assemble() {
+            return View();
+        }
+    }
+}
