77445408e611f75e71ebc6083db118432454362a
朋朋 王 <879173660@qq.com>
2019-12-19 17:01:50 +0800

    zz

---
+++ WebApplication1/Controllers/PdskcController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCM_System.UI.Controllers
+{
+    public class PdskcController : Controller
+    {
+        // GET: Pdskc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Assemble()
+        {
+            return View();
+        }
+
+        public ActionResult AssembleAdd()
+        {
+            return View();
+        }
+
+        public ActionResult AssembleEdit()
+        {
+            return View();
+        }
+
+        public ActionResult CheckDepot()
+        {
+            int i = 1;
+            ViewBag.a = i;
+            return View();
+        }
+
+        public ActionResult CheckDepotAdd()
+        {
+            return View();
+        }
+
+        public ActionResult CheckDepotDetail()
+        {
+            return View();
+        }
+
+        public ActionResult CheckDepotEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Devolves()
+        {
+            return View();
+        }
+
+        public ActionResult DevolvesAdd()
+        {
+            return View();
+        }
+
+        public ActionResult DevolvesDetail()
+        {
+            return View();
+        }
+
+        public ActionResult DevolvesEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Losts()
+        {
+            return View();
+        }
+
+        public ActionResult LostsAdd()
+        {
+            return View();
+        }
+
+        public ActionResult LostsDetail()
+        {
+            return View();
+        }
+
+        public ActionResult LostsEdit()
+        {
+            return View();
+        }
+        public ActionResult PayOff()
+        {
+            return View();
+        }
+
+        public ActionResult PayOffAdd()
+        {
+            return View();
+        }
+
+        public ActionResult PayOffDetail()
+        {
+            return View();
+        }
+
+        public ActionResult PayOffEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Split()
+        {
+            return View();
+        }
+
+        public ActionResult SplitAdd()
+        {
+            return View();
+        }
+
+        public ActionResult SplitDetail()
+        {
+            return View();
+        }
+
+        public ActionResult SplitEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Stream()
+        {
+            return View();
+        }
+    }
+}
